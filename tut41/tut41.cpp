292655f0d7f13ef8b136bb0a8f9986277c116dea
Osicool <1joshalmighty@gmail.com>
2021-07-31 23:01:21 +0100

    tutorial 41

---
+++ tut41/tut41.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int answer = 11;
+    std::cout << "Guess the number: ";
+    int guess;
+    std::cin >> guess;
+    int points = guess  == answer ? 10 : 0;
+
+    std::cout << points << std::endl;
+}
