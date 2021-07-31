af609fa6dd3c009310a0ecd2d6be6bdc702442c3
Osicool <1joshalmighty@gmail.com>
2021-07-31 17:52:08 +0100

    tutorial 5

---
+++ tut5/tut5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std; //using directive
+
+int main(){
+    cout << "This is using the directive so no namespace!\n";
+    std::cout << "However, using the namespace is better because it means your really specific and may avoid naming conflicts!\n";
+    return 0;
+}
