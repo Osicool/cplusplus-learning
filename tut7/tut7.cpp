4387eecf8513ea05113d7df4b616945f893c28be
Osicool <1joshalmighty@gmail.com>
2021-07-31 18:17:17 +0100

    tutorial 7

---
+++ tut7/tut7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int pizzaSlices;
+    std::cout << "Hey big boy, how many pizza slices do you want?" << std::endl;
+    std::cin >> pizzaSlices;
+    std::cout << "You have " << pizzaSlices << " pizza slices." << std::endl;
+}
