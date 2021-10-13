7875910c7339e1691370d1c1b8fecd1c22c578ca
Osicool <1joshalmighty@gmail.com>
2021-10-13 19:44:14 +0100

    tutorial 88 added

---
+++ tut88/tut88.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class User {
+    int user_id = 69;
+
+    public:
+        std::string first_name;
+        std::string last_name;
+        int get_userId(){
+            return user_id;
+        }
+
+        User(){
+            std::cout << "Constructor" << std::endl;
+        }
+
+        User(std::string first_name, std::string last_name, int user_id){
+            this->first_name = first_name;
+            this->last_name = last_name;
+            this->user_id = user_id;
+        }
+};
+
+int main(){
+    User user("Josh", "Bowles", 21);
+    std::cout << "First Name: " << user.first_name << "\tLast Name: " << user.last_name << "\tUser Identifer: " << user.get_userId() << std::endl;
+}
