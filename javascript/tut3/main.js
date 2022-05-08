73b6299e8af8b9d740fb33da80886549789fe17b
Josh Bowles <josh@pop-os.localdomain>
2022-05-08 11:00:19 +0100

    changes

---
+++ javascript/tut3/main.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order now confirmed!"
+    document.getElementById("button").style.display = "none";
+}
